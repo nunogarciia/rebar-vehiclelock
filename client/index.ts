e8aecc116d36e4ebdd5dc68be85702b5f24984b3
KC0d <killer-coder@hotmail.com>
2024-07-25 19:32:39 +0100

    Change the lock/unlock sounds

--- client/index.ts
+++ client/index.ts
@@ -2,22 +2,12 @@ import * as alt from 'alt-client';
 import * as native from 'natives';
 import { VehicleLockEvents } from '../shared/events.js';
 
-alt.onServer(VehicleLockEvents.toClient.playAnim, async (player: alt.Player, vehicle: alt.Vehicle) => {
-    native.playSoundFromEntity(
-        native.getSoundId(),
-        'Bomb_Countdown_Beep',
-        vehicle,
-        'DLC_MPSUM2_ULP2_Rogue_Drones',
-        true,
-        0,
-    );
-    await alt.Utils.wait(350);
-    native.playSoundFromEntity(
-        native.getSoundId(),
-        'Bomb_Countdown_Beep',
-        vehicle,
-        'DLC_MPSUM2_ULP2_Rogue_Drones',
-        true,
-        0,
-    );
-});
+alt.onServer(
+    VehicleLockEvents.toClient.playAnim,
+    async (player: alt.Player, vehicle: alt.Vehicle, soundName: string) => {
+        const sound = soundName.split(' ');
+        native.playSoundFromEntity(native.getSoundId(), sound[0], vehicle, sound[1], true, 0);
+        await alt.Utils.wait(350);
+        native.playSoundFromEntity(native.getSoundId(), sound[0], vehicle, sound[1], true, 0);
+    },
+);
